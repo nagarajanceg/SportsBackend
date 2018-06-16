341fb10b6fafda007c38c061e9afc4040ddee209
nagarajanceg <nagharajang@gmail.com>
2018-06-15 21:26:14 -0500

    Heroku mongodb url changed again

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 var config = {
     db :{
-        url : 'mongodb://nagharajang@gmail.com:Heroku@123@ds261450.mlab.com:61450/heroku_s9v4vx5n',
+        url : process.env.MONGODB_URI,
         // url : 'mongodb://localhost/sports'
     }
 }
